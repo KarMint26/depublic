e3fcc29573aeb42b8474ceb5118e3ed36b7b3f30
karel <kareltrisnanto26@gmail.com>
2023-12-02 23:59:38 +0700

    style(ui_detail_pembayaran): add style to detail pembayaran

--- components/custom/CardMiniTicket.jsx
+++ components/custom/CardMiniTicket.jsx
@@ -7,7 +7,7 @@ export default function CardMiniTicket() {
     <div className="flex w-full justify-between items-center cursor-pointer">
       <div className="flex justify-center items-center gap-2">
         <div className="bg-[#FBF6EC] rounded-full p-2">
-          <Image src="/assets/features/features-1.png" width={30} height={30} />
+          <Image src="/assets/features/features-1.png" width={30} height={30} alt="ticket-icon" />
         </div>
         <div className="flex flex-col">
           <h1 className="text-[0.65rem] Mobile-M:text-[0.75rem] font-semibold">
