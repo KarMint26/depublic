658c25b923b560e6347e6dfbccda623711566a1b
karel <kareltrisnanto26@gmail.com>
2024-01-11 13:30:57 +0700

    fix: bug refresh

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
-  // tambahkan opsi lainnya sesuai kebutuhan Anda
  });
  
  module.exports = withPWA({
